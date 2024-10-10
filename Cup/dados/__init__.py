859b892b8c5cab4fd8036ae3d7f4e44d8ff752ee
Kaio Bitencourt <kaiocesar1993@hotmail.com>
2024-10-09 22:57:25 -0300

    Melhorias no projeto

--- Cup/dados/__init__.py
+++ Cup/dados/__init__.py
@@ -1,6 +1,6 @@
 from random import choice, randint
 from time import sleep
-# iJogos = list()
+from prettytable import PrettyTable
 
 iClassificao = {
     'iGrupoP1': [], 'iGrupoP2': [], 'iGrupoP3': [], 'iGrupoP4': [],
@@ -37,37 +37,47 @@ def iSelecao():
 
 
 def iTabelas():
+    tabela = PrettyTable()
     for c in range(1, 9):
-        print(f'POTE {c} |')
+        tabela.clear()
+        tabela.field_names = [f"Grupo{c}"]
         for k in iClassificao[f'iGrupoP{c}']:
-            print(k)
+            tabela.add_row([k])
+        print(tabela)
             # sleep(0.5)
 
 
 def iGrupo_Sel(iSelecao):
-    for k, v in iClassificao.items():
-        if iSelecao in v:
-            print(f'A Seleção do {iSelecao} esta no Grupo {k[7]}', end='')
-            print(f' e enfrentará as selecoes: ', end='')
-            for j in v:
-                if iSelecao in j:
-                    continue
-                else:
-                    print(f'{j}, ', end='')
-            print()
-            iGrupoCopa()
-            return True
+    tabela = PrettyTable()
+    
+    for c in range (1,9):
+        for selecao in iClassificao[f'iGrupoP{c}']:
+            if iSelecao in selecao:
+                grupo = c
+                for k in iClassificao[f'iGrupoP{grupo}']:
+                    if k in iSelecao:
+                        tabela.field_names = ["Selecao"]
+                        tabela.add_row([iSelecao])
+                        print(tabela)
+                        tabela.clear()
+                    else:
+                        tabela.field_names = ["Adversários"]
+                        tabela.add_row([k])
+                print(tabela)
+                iGrupoCopa()
+                return True
 
 
 def iGrupoCopa():
     Placar1 = 0
     Placar2 = 0
     iCount = 0
+    iPartida = 0
     Principal = list()
 
     for c in range(1, 9):
         print(f'Jogos do Grupo {c}')
-        Principal = iClassificao[f'iGrupoP{c}']
+        Principal = iClassificao[f'iGrupoP{c}']    
         while True:
             for v in range(0, 4):
                 iCount = 0
@@ -78,16 +88,24 @@ def iGrupoCopa():
                     elif Sel_Principal in k:
                         continue
                     else:
+                        iPartida += 1
                         Placar1 = randint(1, 4)
                         Placar2 = randint(1, 4)
                         print(f'{Sel_Principal} {Placar1} x {Placar2} {k}')
                         iJogos[f'iGrupoP{c}'].append([Sel_Principal, k])
                         iPontuacao(Sel_Principal, k, Placar1, Placar2, c)
                         iCount += 1
-                        sleep(0.1)
+                        #sleep(0.1)
                     if iCount == 2:
                         break
+                    if iPartida == 6:
+                        iOrganiza(c)
+                        iPartida = 0
+            
             break
+    print('Classificação final')
+    iApresenta()
+    
 
 
 def iCompara(Nome1, Nome2, Count):
@@ -106,31 +124,90 @@ def iCompara(Nome1, Nome2, Count):
 def iPontuacao(Nome1, Nome2, Placar1, Placar2, Count):
     iVitoria = 3
     iEmpate = 1
-    iDerrota = 0
-    Indice = 0
     Paises = []
 
     if len(Pontuacao_Grupo[f'iGrupoP{Count}']) == 0:
         Paises = (iClassificao[f'iGrupoP{Count}'])
         for c in Paises:
-            Pontuacao_Grupo[f'iGrupoP{Count}'].append([c, 0])
+            Pontuacao_Grupo[f'iGrupoP{Count}'].append([ c, 0, 'iVitoria', 0 , 'Empate' ,0  , 'Derrota', 0 , 'iGols_Feitos', 0, 'iGols_Sofridos' , 0, 'Saldo', 0])
             if c == 4:
                 break
         
-
     for i, item in enumerate(Pontuacao_Grupo[f'iGrupoP{Count}']):
-        if Placar1 > Placar2:
-            if item[0] in Nome1:
+        if item[0] in Nome1:
+            if Placar1 > Placar2:
                 Pontuacao_Grupo[f'iGrupoP{Count}'][i][1] += iVitoria
-                break
-        elif Placar2 > Placar1:
-            if item[0] in Nome2:
-                Pontuacao_Grupo[f'iGrupoP{Count}'][i][1] += iVitoria
-                break
-        elif Placar1 == Placar2:
-            if item[0] in Nome1:
-               Pontuacao_Grupo[f'iGrupoP{Count}'][i][1] += iEmpate
-            if item[0] in Nome2:
+                Pontuacao_Grupo[f'iGrupoP{Count}'][i][3] += 1
+            elif Placar1 == Placar2:
                 Pontuacao_Grupo[f'iGrupoP{Count}'][i][1] += iEmpate
+                Pontuacao_Grupo[f'iGrupoP{Count}'][i][5] += 1
+            else:
+                Pontuacao_Grupo[f'iGrupoP{Count}'][i][7] += 1
+
+            Pontuacao_Grupo[f'iGrupoP{Count}'][i][9] += Placar1
+            Pontuacao_Grupo[f'iGrupoP{Count}'][i][11] += Placar2
+            Pontuacao_Grupo[f'iGrupoP{Count}'][i][13] += (Placar1 - Placar2)
+        elif item[0] in Nome2:
+            if Placar2 > Placar1:
+                Pontuacao_Grupo[f'iGrupoP{Count}'][i][1] += iVitoria
+                Pontuacao_Grupo[f'iGrupoP{Count}'][i][3] += 1
+            elif Placar1 == Placar2:
+                Pontuacao_Grupo[f'iGrupoP{Count}'][i][1] += iEmpate
+                Pontuacao_Grupo[f'iGrupoP{Count}'][i][5] += 1
+            else:
+                Pontuacao_Grupo[f'iGrupoP{Count}'][i][7] += 1
+
+            Pontuacao_Grupo[f'iGrupoP{Count}'][i][9] += Placar2
+            Pontuacao_Grupo[f'iGrupoP{Count}'][i][11] += Placar1
+            Pontuacao_Grupo[f'iGrupoP{Count}'][i][13] += (Placar2 - Placar1)
+
+
+def iOrganiza(c):
+    grupo_key = f'iGrupoP{c}'
+    selecoes = Pontuacao_Grupo.get(grupo_key, [])
+    selecoes_ordenadas = sorted(selecoes, key=lambda item: (item[1], item[3], item[13], item[9] ), reverse=True)
+    Pontuacao_Grupo[grupo_key] = []
+
+    for j in range(0, 4):
+        for k in selecoes_ordenadas:
+            Pontuacao_Grupo[grupo_key].append(k)
+        break
+
+def extrair_valores(dados):
+        return [dados[0], dados[1], dados[3], dados[5], dados[7], dados[9], dados[11], dados[13]]
+
+
+def iApresenta():
+    tabela = PrettyTable()
+    for k in range(1,9):
+        tabela.clear()
+        tabela.field_names = ["Classificação", "P", "V", "E", "D", "GP", "GC", "SG"]
+        iPosicao = 0
+        selecao = []
+        print(f'POTE {k} |')
+        for selecao in Pontuacao_Grupo[f'iGrupoP{k}']:
+            statistica = extrair_valores(selecao)
+            tabela.add_row([f"{iPosicao+1}: {statistica[0]}"] + statistica[1:])
+            iPosicao += 1
+            if iPosicao == 4:
                 break
-            
+        print(tabela)
+
+        
+
+
+# def iGrupo_Sel(iSelecao):
+#     tabela = PrettyTable()
+#     for k, v in iClassificao.items():
+#         tabela.field_names = ["Selecao", "Adversários"]
+#         if iSelecao in v:
+#             print(f'A Seleção do {iSelecao} esta no Grupo {k[7]}', end='')
+#             print(f' e enfrentará as selecoes: ', end='')
+#             for j in v:
+#                 if iSelecao in j:
+#                     continue
+#                 else:
+#                     print(f'{j}, ', end='')
+#             print()
+#             iGrupoCopa()
+#             return True
--- World.py
+++ World.py
@@ -3,6 +3,7 @@ from Cup import dados
 dados.iSelecao()
 
 while True:
-    iEscolha = str(input('Escolha a sua selecao: ')).lower()
+    #iEscolha = str(input('Escolha a sua selecao: ')).lower()
+    iEscolha = "Colombia"
     if dados.iGrupo_Sel(iEscolha.capitalize()):
         break
